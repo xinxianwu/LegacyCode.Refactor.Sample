625f27a23d256f544d89a1b351c736c1f851a715
xinxian.wu <xinxian.wu@onelab.tw>
2025-02-26 16:10:04 +0800

    Add initial project structure with configuration and weather forecast API

---
+++ LegacyCode.Refactor.Sample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using LegacyCode.Refactor.Sample.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegacyCode.Refactor.Sample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var weatherService = new WeatherService();
+        weatherService.GetWeatherForecast(DateTime.Now);
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
---
+++ LegacyCode.Refactor.Sample/Services/WeatherService.cs
@@ -0,0 +1,26 @@
+namespace LegacyCode.Refactor.Sample.Services;
+
+public class WeatherService
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    public WeatherForecast GetWeatherForecast(DateTime now)
+    {
+        var queryTemperature = QueryTemperature(now);
+
+        return new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(now),
+            TemperatureC = queryTemperature,
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+    }
+
+    private int QueryTemperature(DateTime dateTime)
+    {
+        return Random.Shared.Next(-20, 55);
+    }
+}
